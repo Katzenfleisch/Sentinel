b529a3962e874944c6c5174a6a97769b93b8aac6
Jean-Baptiste <jeanbaptiste.laurent.pro@gmail.com>
2017-12-08 00:04:53 +0100

    Adding a few utils, and fix a call limit if the time is 0

--- Sentinel/lua/sntl/sntl_Utils.lua
+++ Sentinel/lua/sntl/sntl_Utils.lua
@@ -1,14 +1,49 @@
 
 local kLimitCallFrequency = {}
 
-function LimitCallFrequency(id, frequency)
+function SNTL_LimitCallFrequency(id, frequency)
     local now = Shared.GetTime()
 
-    kLimitCallFrequency[id] = kLimitCallFrequency[id] or 0
-    if kLimitCallFrequency[id] >= now then
-        return true
+    if kLimitCallFrequency[id] then
+        if kLimitCallFrequency[id] >= now then
+            return true
+        end
+        kLimitCallFrequency[id] = now + frequency
+    else
+        kLimitCallFrequency[id] = 0
     end
-    kLimitCallFrequency[id] = now + frequency
-
     return false
+end
+
+function SNTL_IsPlayerVirtual(ent)
+    if (ent and ent.GetClient and ent:GetClient() and not ent:GetClient():GetIsVirtual()) then
+      return false
+   else
+      return true
+   end
+end
+
+-- Needed for bots to have a uniq ID, otherwise they would cound as '1' player.
+-- This will, for example, make all bot gorges shared the same tunnel/hydras/clogs pool.
+if Server then
+    local botsClientIdIt = -1
+    local botsClientIds = {}
+    local ServerClientGetUserId = ServerClient.GetUserId
+    local function SNTL_GetUserId(self)
+        local userId = ServerClientGetUserId(self)
+        if userId == 0 then
+            local player = self:GetControllingPlayer()
+
+            if player then
+                if not botsClientIds[player:GetName()] then
+                    botsClientIds[player:GetName()] = botsClientIdIt
+                    botsClientIdIt = botsClientIdIt - 1
+                end
+                return botsClientIds[player:GetName()]
+            end
+        end
+        return userId
+    end
+
+    ServerClient.GetUserId = SNTL_GetUserId
 end
