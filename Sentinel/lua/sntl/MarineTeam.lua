be2e4a458c6f8645fbd90ef039bbf61612e1b4ed
Jean-Baptiste <jeanbaptiste.laurent.pro@gmail.com>
2017-12-10 00:34:27 +0100

    Adding other buildings to the marine team, and ensure they don't overlap

--- Sentinel/lua/sntl/MarineTeam.lua
+++ Sentinel/lua/sntl/MarineTeam.lua
@@ -1,9 +1,33 @@
+local function SetRandomAngle(ent)
+    -- Randomize starting angles
+    local angles = ent:GetAngles()
+    angles.yaw = math.random() * math.pi * 2
+    ent:SetAngles(angles)
+
+    -- To make sure physics model is updated without waiting a tick
+    ent:UpdatePhysicsModel()
+end
+
 function MarineTeam:SpawnInitialStructures(techPoint)
 
     assert(techPoint ~= nil)
 
-    local ip = CreateEntity(InfantryPortal.kMapName, techPoint:GetOrigin(), kMarineTeamType)
+    local origin = techPoint:GetOrigin()
+    local ip = CreateEntity(InfantryPortal.kMapName, origin, kMarineTeamType)
     ip:SetConstructionComplete()
+
+    -- Force buildings to be on a different circle around the IP so they don't overlap
+    local pg_origin      = SNTL_SpreadedPlacementFromOrigin(GetExtents(kTechId.PhaseGate),  origin, 1, 2, 3)
+    local armslab_origin = SNTL_SpreadedPlacementFromOrigin(GetExtents(kTechId.ArmsLab),    origin, 1, 5, 6)
+    local armory_origin  = SNTL_SpreadedPlacementFromOrigin(GetExtents(kTechId.Armory),     origin, 1, 5, 6)
+
+    local pg      = pg_origin       and CreateEntity(PhaseGate.kMapName,    pg_origin[1],       kMarineTeamType)
+    local armslab = armslab_origin  and CreateEntity(ArmsLab.kMapName,      armslab_origin[1],  kMarineTeamType)
+    local armory  = armory_origin   and CreateEntity(Armory.kMapName,       armory_origin[1],   kMarineTeamType)
+
+    if pg       then pg:SetConstructionComplete()       ; SetRandomAngle(pg)        end
+    if armslab  then armslab:SetConstructionComplete()  ; SetRandomAngle(armslab)   end
+    if armory   then armory:SetConstructionComplete()   ; SetRandomAngle(armory)    end
 
     return
 
