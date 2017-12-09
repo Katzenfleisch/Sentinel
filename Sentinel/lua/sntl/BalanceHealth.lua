2499bcb61b11a8039d79fa91d305bf964eae733d
Jean-Baptiste <jeanbaptiste.laurent.pro@gmail.com>
2017-12-09 23:20:43 +0100

    Reduced the health of the eggs

---
+++ Sentinel/lua/sntl/BalanceHealth.lua
@@ -0,0 +1,6 @@
+
+kEggHealth = 350/2.5
+kEggArmor = 0
+
+kMatureEggHealth = 200/2.5
+kMatureEggArmor = 0
--- Sentinel/lua/sntl/FileHooks.lua
+++ Sentinel/lua/sntl/FileHooks.lua
@@ -11,6 +11,8 @@ ModLoader_SetupFileHook("lua/Team.lua", "post")
 ModLoader_SetupFileHook("lua/AlienTeam.lua", "post")
 ModLoader_SetupFileHook("lua/PlayingTeam.lua", "post")
 
+ModLoader_SetupFileHook("lua/BalanceHealth.lua", "post")
+
 ModLoader_SetupFileHook("lua/Egg.lua", "post")
 ModLoader_SetupFileHook("lua/Player.lua", "post")
 ModLoader_SetupFileHook("lua/bots/BotTeamController.lua", "post")
