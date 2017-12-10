78196f3ef27f7e62d087c999d5a279cb650ee330
Jean-Baptiste <jeanbaptiste.laurent.pro@gmail.com>
2017-12-10 03:26:35 +0100

    Adding a check at alien spawn to reuse the player capsule to prevent stuck issues

--- Sentinel/lua/sntl/Egg.lua
+++ Sentinel/lua/sntl/Egg.lua
@@ -30,6 +30,28 @@ function Egg:OnInitialized()
 
     local rval = old_Egg_OnInitialized and old_Egg_OnInitialized(self)
     return rval
+
 end
+
+local old_Egg_SpawnPlayer = Egg.SpawnPlayer
+function Egg:SpawnPlayer(player)
+
+    local rval, new_player = old_Egg_SpawnPlayer(self, player)
+
+    if rval then
+
+        -- Increase the extents a bit to be REALLY SURE not to get stuck because one nail is in the ground
+        local extents = LookupTechData(new_player:GetTechId(), kTechDataMaxExtents) * 1.25
+        local position = new_player:GetOrigin()
+
+        position = SNTL_GetGroundAtPosition(position, nil, PhysicsMask.AllButPCs, extents)
+        new_player:SetOrigin(position)
+
+    end
+
+    return rval, new_player
+
+end
+
 
 Shared.LinkClassToMap("Egg", nil, networkVars)
