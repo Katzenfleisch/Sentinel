62c5364fb0d6d851315d536eb8d8bc186fd7a82c
Jean-Baptiste <jeanbaptiste.laurent.pro@gmail.com>
2017-12-17 02:32:40 +0100

    Reworked balance a bit

--- Sentinel/lua/sntl/Balance.lua
+++ Sentinel/lua/sntl/Balance.lua
@@ -1,6 +1,6 @@
-kAlienSpawnTime = 20
+kAlienSpawnTime = 15
 kMarineRespawnTime = 5--9
 
 -- 45 is enough for 1 SG (20), 1 Jetpack (15) and several minor stuff (or 1 exo)
-kMarineInitialIndivRes = 45 -- 15
+kMarineInitialIndivRes = 50 -- 15
 
