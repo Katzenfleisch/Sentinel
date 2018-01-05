30a5985651629d44bc2fe494a1e6e065fb8cf9b0
Jean-Baptiste <jeanbaptiste.laurent.pro@gmail.com>
2018-01-05 18:14:43 +0100

    Changed the bot prefix so people now this is a custom AI

--- Sentinel/lua/sntl/FileHooks.lua
+++ Sentinel/lua/sntl/FileHooks.lua
@@ -37,6 +37,7 @@ ModLoader_SetupFileHook("lua/NS2Utility.lua", "post")
 ModLoader_SetupFileHook("lua/Egg.lua", "post")
 ModLoader_SetupFileHook("lua/Player.lua", "post")
 
+ModLoader_SetupFileHook("lua/bots/PlayerBot.lua", "post")
 ModLoader_SetupFileHook("lua/bots/TeamBrain.lua", "post")
 ModLoader_SetupFileHook("lua/bots/BotMotion.lua", "replace")
 ModLoader_SetupFileHook("lua/bots/SkulkBrain_Data.lua", "replace")
