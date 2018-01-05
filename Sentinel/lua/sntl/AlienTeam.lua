3a1e5ed9075fad285b9fc30a3faee154c00d3bc4
Jean-Baptiste <jeanbaptiste.laurent.pro@gmail.com>
2018-01-05 16:18:59 +0100

    Moar babblers

--- Sentinel/lua/sntl/AlienTeam.lua
+++ Sentinel/lua/sntl/AlienTeam.lua
@@ -252,7 +252,7 @@ local old_AlienTeam_SpawnInitialStructures = AlienTeam.SpawnInitialStructures
 function AlienTeam:SpawnInitialStructures(techPoint)
     local numHydras = 3
     local numWebs = 20
-    local babblerEggs = 7
+    local babblerEggs = 8
     local kNumEggGroup = 4
     local kNumEggPerGroup = 6
     local _, eggs = SpawnRandomEggs(kNumEggGroup, kNumEggPerGroup, techPoint:GetOrigin())
